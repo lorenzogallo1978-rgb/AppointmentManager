0154f91596dffd45253b4b65a9888e58de09776d
lorenzogallo1978-rgb <lorenzogallo1978@gmail.com>
2026-09-24 20:44:48 +0300

    Create Color.kt

---
+++ app/src/main/java/com/appointmentmanager/app/ui/theme/Color.kt
@@ -0,0 +1,13 @@
+package com.appointmentmanager.app.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val AppPrimary = Color(0xFF006C4C)
+val AppOnPrimary = Color(0xFFFFFFFF)
+val AppSecondary = Color(0xFF4D6358)
+val AppTertiary = Color(0xFF806000)
+
+val AppDarkPrimary = Color(0xFF69DBAE)
+val AppDarkOnPrimary = Color(0xFF003827)
+val AppDarkSecondary = Color(0xFFB5CCBE)
+val AppDarkTertiary = Color(0xFFE8C46A)
