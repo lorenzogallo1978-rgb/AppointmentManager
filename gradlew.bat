3a161f7a905dc21ae5d4881000fcf205285d59ac
lorenzogallo1978-rgb <lorenzogallo1978@gmail.com>
2026-09-24 20:32:56 +0300

    Update gradlew.bat

--- gradlew.bat
+++ gradlew.bat
@@ -1,70 +1,23 @@
-@rem
-@rem Copyright 2015 the original author or authors.
-@rem
-@rem Licensed under the Apache License, Version 2.0 (the "License");
-@rem you may not use this file except in compliance with the License.
-@rem You may obtain a copy of the License at
-@rem
-@rem      https://www.apache.org/licenses/LICENSE-2.0
-@rem
-@rem Unless required by applicable law or agreed to in writing, software
-@rem distributed under the License is distributed on an "AS IS" BASIS,
-@rem WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-@rem See the License for the specific language governing permissions and
-@rem limitations under the License.
-@rem
+@echo off
+setlocal
 
-@if "%DEBUG%"=="" @echo off
-@rem ##########################################################################
-@rem
-@rem  Gradle startup script for Windows
-@rem
-@rem ##########################################################################
+set "APP_HOME=%~dp0"
+set "WRAPPER_JAR=%APP_HOME%gradle\wrapper\gradle-wrapper.jar"
 
-set DIRNAME=%~dp0
-if "%DIRNAME%"=="" set DIRNAME=.
-set APP_BASE_NAME=%~n0
-set APP_HOME=%DIRNAME%
-
-set DEFAULT_JVM_OPTS="-Xmx64m" "-Xms64m"
-
-set CLASSPATH=%APP_HOME%\gradle\wrapper\gradle-wrapper.jar
-
-if not exist "%CLASSPATH%" (
+if not exist "%WRAPPER_JAR%" (
     echo ERROR: gradle\wrapper\gradle-wrapper.jar is missing.
-    echo Download it from:
-    echo https://github.com/gradle/gradle/raw/v8.7.0/gradle/wrapper/gradle-wrapper.jar
+    echo GitHub Actions downloads this file automatically in the supplied workflow.
+    echo For local builds, download it from the Gradle 8.7 source as described in the guide.
     exit /b 1
 )
 
-if defined JAVA_HOME goto findJavaFromJavaHome
+if defined JAVA_HOME (
+    set "JAVA_EXE=%JAVA_HOME%\bin\java.exe"
+) else (
+    set "JAVA_EXE=java.exe"
+)
 
-set JAVA_EXE=java.exe
-%JAVA_EXE% -version >NUL 2>&1
-if "%ERRORLEVEL%"=="0" goto execute
+"%JAVA_EXE%" -classpath "%WRAPPER_JAR%" org.gradle.wrapper.GradleWrapperMain %*
+set "EXIT_CODE=%ERRORLEVEL%"
 
-echo ERROR: JAVA_HOME is not set and no 'java' command could be found in your PATH.
-goto fail
-
-:findJavaFromJavaHome
-set JAVA_HOME=%JAVA_HOME:"=%
-set JAVA_EXE=%JAVA_HOME%/bin/java.exe
-
-if exist "%JAVA_EXE%" goto execute
-
-echo ERROR: JAVA_HOME is set to an invalid directory: %JAVA_HOME%
-echo Please set the JAVA_HOME variable in your environment to match the
-echo location of your Java installation.
-goto fail
-
-:execute
-"%JAVA_EXE%" %DEFAULT_JVM_OPTS% %JAVA_OPTS% %GRADLE_OPTS% "-Dorg.gradle.appname=%APP_BASE_NAME%" -classpath "%CLASSPATH%" org.gradle.wrapper.GradleWrapperMain %*
-
-:end
-if "%OS%"=="Windows_NT" endlocal
-
-:fail
-rem Set variable GRADLE_EXIT_CONSOLE if you need the _script_ return code instead of
-rem the _cmd.exe /c return code!
-if not ""=="%GRADLE_EXIT_CONSOLE%" exit 1
-exit /b 1
+endlocal & exit /b %EXIT_CODE%
